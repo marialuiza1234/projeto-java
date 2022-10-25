c7ceb659d55475b8c3ac1b36da9833e382366b1c
Maria Luiza <luiza.mallu1@gmail.com>
2022-10-24 21:40:31 -0300

    First class: Position

--- application/Program.java
+++ application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Projeto curso java");
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 
 	}
 
